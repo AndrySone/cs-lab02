d8527af4f66ccf2eb9036f14d7417d5820273d9c
Alice (YakovlevAM) <iakovlevandry@gmail.com>
2024-06-24 23:26:38 +0300

    code: заготовка программы

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ int main()
     int a, b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
-         << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
     return 0;
 }
